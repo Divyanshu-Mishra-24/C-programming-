291d36f3aa998abad145974388c3c527a6a3499f
Divyxshu <148748230+Divyanshu-Mishra-24@users.noreply.github.com>
2024-09-10 19:34:14 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int factorial(int x)
+{
+    if (x==0) return 1 ;
+    int a=x*factorial(x-1);
+    return a;
+}
+int main ()
+{  
+    int n;
+    printf("ENter n=");
+    scanf("%d",&n);
+    int a=factorial(n);
+    printf("%d",a);
+    return 0;
+}
